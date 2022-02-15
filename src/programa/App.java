7085bdfe56a2d6d106bf567ff1c8df8ff9914e2c
Thiago <thiagocoelho.coelho2@gmail.com>
2022-02-15 19:42:01 -0300

    Estabelecendo conexao com o banco de dados

--- src/programa/App.java
+++ src/programa/App.java
@@ -1,5 +1,14 @@
+package programa;
+
+import java.sql.Connection;
+
+import modelo.database.DataBase;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args)throws Exception {
+        Connection conn =DataBase.getConnection(); 
+        System.out.print("Ok");
+        DataBase.closeConnection();
+        
     }
 }
