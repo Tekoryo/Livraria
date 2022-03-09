ca3af5780e665ce432c387b4db26dd0e9852ba9c
Thiago <thiagocoelho.coelho2@gmail.com>
2022-03-09 16:32:10 -0300

    e

--- src/modelo/implementacao/LivrosDAOJDBC.java
+++ src/modelo/implementacao/LivrosDAOJDBC.java
@@ -15,12 +15,13 @@ import modelo.database.DbException;
 public class LivrosDAOJDBC implements LivrosDAO {
 
     private Connection conn;
-    
-    public LivrosDAOJDBC(Connection conn){
-        this.conn=conn;
-        
+
+    public LivrosDAOJDBC(Connection conn) {
+        this.conn = conn;
+
     }
-    public String TestStatus(){
+
+    public String TestStatus() {
         String status;
         if (conn != null) {
 
@@ -34,38 +35,44 @@ public class LivrosDAOJDBC implements LivrosDAO {
         return status;
 
     }
+
     @Override
     public void Incerindo(Livros obj) {
-        PreparedStatement st=null;
-        try{
-            st=conn.prepareStatement(
-                "INSERT INTO LIVROS(NOMEDOLIVRO,NOMEDOAUTOR,SEXO,NUMERODEPAGINAS,NOMEDAEDITORA,VALORDOLIVRO,UF,ANOPUBLICACAO)VALUES(?,?,?,?,?,?,?,?)",Statement.RETURN_GENERATED_KEYS);   
-            st.setString(1,obj.getNomeDoLivro());
-            st.setString(2,obj.getNomeDoAutor());
-            st.setString(3,obj.getSexo());
+        PreparedStatement st = null;
+        try {
+            st = conn.prepareStatement(
+                    "INSERT INTO LIVROS(NOMEDOLIVRO,NOMEDOAUTOR,SEXO,NUMERODEPAGINAS,NOMEDAEDITORA,VALORDOLIVRO,UF,ANOPUBLICACAO)VALUES(?,?,?,?,?,?,?,?)",
+                    Statement.RETURN_GENERATED_KEYS);
+            st.setString(1, obj.getNomeDoLivro());
+            st.setString(2, obj.getNomeDoAutor());
+            st.setString(3, obj.getSexo());
             st.setInt(4, obj.getNumeeoDePaginas());
-            st.setString(5,obj.getNomeDaEditora());
+            st.setString(5, obj.getNomeDaEditora());
             st.setDouble(6, obj.getValorDoLivro());
-            st.setString(7,obj.getUF());
+            st.setString(7, obj.getUF());
             st.setInt(8, obj.getAnoPublicacao());
 
             int rowsAffected = st.executeUpdate();
-			System.out.println("\n"+rowsAffected);
-        }
-        catch(SQLException e){
-            System.out.print("[ERRO] "+e.getMessage());
+            System.out.println("\n" + rowsAffected);
+        } catch (SQLException e) {
+            System.out.print("[ERRO] " + e.getMessage());
         }
     }
 
     @Override
     public void Update(Livros obj) {
         // TODO Auto-generated method stub
-        
+
     }
 
     @Override
     public void Remove(Livros obj) {
-        // TODO Auto-generated method stub
+        try{
+            st=conn.prepareStatement("arg0");
+        }
+        catch(SQLException e){
+            throw new DbException(e.getMessage());
+        }
         
     }
 
@@ -75,5 +82,4 @@ public class LivrosDAOJDBC implements LivrosDAO {
         return null;
     }
 
-    
 }
